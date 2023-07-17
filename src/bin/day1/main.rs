8ae77a891cc7bf07bc1c633858bee6fbcd210033
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2023-07-17 16:28:08 +0000

    Day 1 part 2

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,18 +1,39 @@
 fn main() {
     let input: Vec<&str> = include_str!("input.txt").split(", ").collect();
     let mut dir: i32 = 0;
-    let mut distance: [i32; 4] = [0; 4];
+    let mut place = (0, 0);
+    let mut visited: Vec<(i32, i32)> = vec![(0, 0)];
 
-    for direction in input {
+    'outer: for direction in input {
         let (d, s) = direction.split_at(1);
-
         dir = (dir + if d == "R" { 1 } else { -1 }).rem_euclid(4);
 
-        distance[dir as usize] += s.parse::<i32>().expect("Should be a number")
-    }
+        let x: &(i32, i32) = &visited.last().cloned().unwrap();
+        let parse = s.parse().expect("Should be a number");
+        for i in 1..=parse {
+            let t = (
+                x.0 + if dir == 1 {
+                    i
+                } else if dir == 3 {
+                    -i
+                } else {
+                    0
+                },
+                x.1 + if dir == 0 {
+                    i
+                } else if dir == 2 {
+                    -i
+                } else {
+                    0
+                },
+            );
 
-    println!(
-        "Distance parcourue: {}",
-        (distance[0]).abs_diff(distance[2]) + (distance[1]).abs_diff(distance[3])
-    )
+            if visited.contains(&t) {
+                place = t;
+                break 'outer;
+            }
+            visited.push(t);
+        }
+    }
+    println!("Distance parcourue: {}", place.0.abs() + place.1.abs())
 }
