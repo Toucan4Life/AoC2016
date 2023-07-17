6e13666a6c9e3b75655550fd8c9d2540edda4bb5
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2023-07-17 15:22:20 +0000

    day 1 part 1

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,4 +1,18 @@
+fn main() {
+    let input: Vec<&str> = include_str!("input.txt").split(", ").collect();
+    let mut dir: i32 = 0;
+    let mut distance: [i32; 4] = [0; 4];
 
-fn main() {   
-    println!("hello world")
+    for direction in input {
+        let (d, s) = direction.split_at(1);
+
+        dir = (dir + if d == "R" { 1 } else { -1 }).rem_euclid(4);
+
+        distance[dir as usize] += s.parse::<i32>().expect("Should be a number")
+    }
+
+    println!(
+        "Distance parcourue: {}",
+        (distance[0]).abs_diff(distance[2]) + (distance[1]).abs_diff(distance[3])
+    )
 }
