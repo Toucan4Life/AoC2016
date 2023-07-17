90d2f89b9143020010d6bdb09cad8bc92dc97a1e
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2023-07-17 17:29:28 +0000

    Day 1 refactor

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,39 +1,56 @@
+use std::collections::HashSet;
+
 fn main() {
-    let input: Vec<&str> = include_str!("input.txt").split(", ").collect();
-    let mut dir: i32 = 0;
-    let mut place = (0, 0);
-    let mut visited: Vec<(i32, i32)> = vec![(0, 0)];
+    let places = include_str!("input.txt")
+        .split(", ")
+        .fold((0, vec![(0, 0)]), |(last_dir, places), direction| {
+            walk(direction, last_dir, places)
+        })
+        .1;
 
-    'outer: for direction in input {
-        let (d, s) = direction.split_at(1);
-        dir = (dir + if d == "R" { 1 } else { -1 }).rem_euclid(4);
+    let mut place: (i32, i32) = places
+        .last()
+        .cloned()
+        .expect("The should be at least one place");
+    println!("Distance parcourue: {}", place.0.abs() + place.1.abs());
 
-        let x: &(i32, i32) = &visited.last().cloned().unwrap();
-        let parse = s.parse().expect("Should be a number");
-        for i in 1..=parse {
-            let t = (
-                x.0 + if dir == 1 {
-                    i
-                } else if dir == 3 {
-                    -i
-                } else {
-                    0
-                },
-                x.1 + if dir == 0 {
-                    i
-                } else if dir == 2 {
-                    -i
-                } else {
-                    0
-                },
-            );
-
-            if visited.contains(&t) {
-                place = t;
-                break 'outer;
-            }
-            visited.push(t);
+    let mut unique_ports = HashSet::new();
+    for visit in places {
+        if !unique_ports.insert(visit) {
+            place = visit;
+            break;
         }
     }
-    println!("Distance parcourue: {}", place.0.abs() + place.1.abs())
+
+    println!("Distance parcourue: {}", place.0.abs() + place.1.abs());
+}
+
+fn walk(direction: &str, dir: i32, mut visited: Vec<(i32, i32)>) -> (i32, Vec<(i32, i32)>) {
+    let (d, s) = direction.split_at(1);
+    let dir = (dir + if d == "R" { 1 } else { -1 }).rem_euclid(4);
+    let last_place = visited.last().cloned().unwrap();
+    let mut vis: Vec<(i32, i32)> = (1..=s.parse().expect("Should be a number"))
+        .map(|i| {
+            (
+                last_place.0
+                    + if dir == 1 {
+                        i
+                    } else if dir == 3 {
+                        -i
+                    } else {
+                        0
+                    },
+                last_place.1
+                    + if dir == 0 {
+                        i
+                    } else if dir == 2 {
+                        -i
+                    } else {
+                        0
+                    },
+            )
+        })
+        .collect();
+    visited.append(&mut vis);
+    (dir, visited)
 }
