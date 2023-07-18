3c0d36a00ccc3269733d01803a2c6e248a18d248
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2023-07-18 07:21:12 +0000

    Day1 benchmark

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,56 +1,28 @@
-use std::collections::HashSet;
-
 fn main() {
-    let places = include_str!("input.txt")
-        .split(", ")
-        .fold((0, vec![(0, 0)]), |(last_dir, places), direction| {
-            walk(direction, last_dir, places)
-        })
-        .1;
+    let mut dir: i32 = 0;
+    let mut last_place: (i32, i32) = (0, 0);
+    let mut visited: Vec<(i32, i32)> = vec![(0, 0)];
+    let change_vectors = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+    
+    'outer: for direction in include_str!("input.txt").split(", ") {
+        let (d, s) = direction.split_at(1);
+        dir = (dir + if d == "R" { 1 } else { -1 }).rem_euclid(4);
+        for _ in 1..=s.parse().expect("Should be a number") {
+            last_place = (
+                last_place.0 + change_vectors[dir as usize].0,
+                last_place.1 + change_vectors[dir as usize].1,
+            );
 
-    let mut place: (i32, i32) = places
-        .last()
-        .cloned()
-        .expect("The should be at least one place");
-    println!("Distance parcourue: {}", place.0.abs() + place.1.abs());
+            if visited.contains(&last_place) {
+                break 'outer;
+            }
 
-    let mut unique_ports = HashSet::new();
-    for visit in places {
-        if !unique_ports.insert(visit) {
-            place = visit;
-            break;
+            visited.push(last_place);
         }
     }
 
-    println!("Distance parcourue: {}", place.0.abs() + place.1.abs());
-}
-
-fn walk(direction: &str, dir: i32, mut visited: Vec<(i32, i32)>) -> (i32, Vec<(i32, i32)>) {
-    let (d, s) = direction.split_at(1);
-    let dir = (dir + if d == "R" { 1 } else { -1 }).rem_euclid(4);
-    let last_place = visited.last().cloned().unwrap();
-    let mut vis: Vec<(i32, i32)> = (1..=s.parse().expect("Should be a number"))
-        .map(|i| {
-            (
-                last_place.0
-                    + if dir == 1 {
-                        i
-                    } else if dir == 3 {
-                        -i
-                    } else {
-                        0
-                    },
-                last_place.1
-                    + if dir == 0 {
-                        i
-                    } else if dir == 2 {
-                        -i
-                    } else {
-                        0
-                    },
-            )
-        })
-        .collect();
-    visited.append(&mut vis);
-    (dir, visited)
+    println!(
+        "Distance parcourue: {}",
+        last_place.0.abs() + last_place.1.abs()
+    )
 }
