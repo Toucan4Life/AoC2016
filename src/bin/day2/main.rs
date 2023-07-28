99a60f7c061aa3632212e535b5a31f7627552732
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2023-07-28 18:13:31 +0000

    day 2 functional

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -1,6 +1,5 @@
-use std::str::FromStr;
+use itertools::Itertools;
 
-#[derive(Debug)]
 enum Direction {
     Left,
     Right,
@@ -8,68 +7,70 @@ enum Direction {
     Down,
 }
 
-impl FromStr for Direction {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+impl From<char> for Direction {
+    fn from(s: char) -> Self {
         match s {
-            "L" => Ok(Self::Left),
-            "R" => Ok(Self::Right),
-            "U" => Ok(Self::Up),
-            "D" => Ok(Self::Down),
-            _ => Err(format!("Can't' parse {}", s)),
+            'L' => Self::Left,
+            'R' => Self::Right,
+            'U' => Self::Up,
+            'D' => Self::Down,
+            _ => todo!(),
         }
     }
 }
 
 struct KeyPad {
-    last_place: [i32;2],
-    keys : [[String;5];5]
+    keys: [[String; 5]; 5],
 }
 
 impl KeyPad {
-    fn new(start_position: [i32;2]) -> Self {
-        Self {
-            last_place: start_position,
-            keys : [[String::from(""),String::from(""),String::from("1"),String::from(""),String::from("")],
-                    [String::from(""),String::from("2"),String::from("3"),String::from("4"),String::from("")],
-                    [String::from("5"),String::from("6"),String::from("7"),String::from("8"),String::from("9")],
-                    [String::from(""),String::from("A"),String::from("B"),String::from("C"),String::from("")],
-                    [String::from(""),String::from(""),String::from("D"),String::from(""),String::from("")]]
+    fn new(keys: [[String; 5]; 5]) -> Self {
+        Self { keys }
+    }
+
+    fn move_to(&mut self, instruction: Direction, last_place: [i32; 2]) -> [i32; 2] {
+        match instruction {
+            Direction::Left  => if let Some(_) = self.try_moving([last_place[0], last_place[1] - 1]) { [last_place[0], last_place[1] - 1] } else { last_place },
+            Direction::Right => if let Some(_) = self.try_moving([last_place[0], last_place[1] + 1]) { [last_place[0], last_place[1] + 1] } else { last_place },
+            Direction::Up    => if let Some(_) = self.try_moving([last_place[0] - 1, last_place[1]]) { [last_place[0] - 1, last_place[1]] } else { last_place },
+            Direction::Down  => if let Some(_) = self.try_moving([last_place[0] + 1, last_place[1]]) { [last_place[0] + 1, last_place[1]] } else { last_place },
         }
     }
-    fn move_to(&mut self, instruction: Direction) {
-        match instruction {
-            Direction::Left => {
-                if self.last_place[1] != 0 && !self.keys.get(self.last_place[0] as usize).unwrap().get((self.last_place[1] - 1) as usize).unwrap().is_empty() {
-                    self.last_place[1] -= 1
-                }
-            }
-            Direction::Right => {
-                if self.last_place[1] != 4 && !self.keys.get(self.last_place[0] as usize).unwrap().get((self.last_place[1] + 1) as usize).unwrap().is_empty() {
-                    self.last_place[1] += 1
-                }
-            }
-            Direction::Up => {
-                if self.last_place[0] != 0 && !self.keys.get((self.last_place[0] - 1) as usize).unwrap().get(self.last_place[1] as usize).unwrap().is_empty() {
-                    self.last_place[0] -= 1
-                }
-            }
-            Direction::Down => {
-                if self.last_place[0] != 4 && !self.keys.get((self.last_place[0] + 1) as usize).unwrap().get(self.last_place[1] as usize).unwrap().is_empty() {
-                    self.last_place[0] += 1
-                }
-            }
+
+    fn try_moving(&mut self, position: [i32; 2]) -> Option<String> {
+        let Some(row) = self.keys.get((position[0]) as usize) else{return None};
+        let Some(digit) = row.get((position[1]) as usize) else{return None};
+        if digit.is_empty() {
+            return None;
         }
+        return Some(digit.to_string());
     }
 }
 
 fn main() {
-    let mut key_pad = KeyPad::new([2,0]);
-    for lines in include_str!("input.txt").lines() {
-        for direction in lines.chars() {
-            key_pad.move_to(Direction::from_str(&direction.to_string()).unwrap())
-        }
-        println!("digit: {}", key_pad.keys.get(key_pad.last_place[0] as usize).unwrap().get(key_pad.last_place[1] as usize).unwrap())
-    }
+    let start_place = [2, 0];
+
+    let mut key_pad = KeyPad::new([
+        [String::from(""),  String::from(""),  String::from("1"), String::from(""),  String::from("")],
+        [String::from(""),  String::from("2"), String::from("3"), String::from("4"), String::from("")],
+        [String::from("5"), String::from("6"), String::from("7"), String::from("8"), String::from("9")],
+        [String::from(""),  String::from("A"), String::from("B"), String::from("C"), String::from("")],
+        [String::from(""),  String::from(""),  String::from("D"), String::from(""),  String::from("")],
+    ]);
+
+    let code = include_str!("input.txt")
+        .lines()
+        .map(|lines| {
+            let digit_position = lines.chars().fold(start_place, |acc, direction| {
+                key_pad.move_to(Direction::from(direction), acc)
+            });
+            return if let Some(digit) = key_pad.try_moving(digit_position) {
+                digit
+            } else {
+                "".to_string()
+            };
+        })
+        .join("");
+
+    println!("{}", code);
 }
