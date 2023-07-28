45e8eaad2772371ea64f2535e9ea5d963c4ce6b2
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2023-07-28 15:13:11 +0000

    Day 2 part 2

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -23,35 +23,41 @@ impl FromStr for Direction {
 }
 
 struct KeyPad {
-    last_place: i32,
+    last_place: [i32;2],
+    keys : [[String;5];5]
 }
 
 impl KeyPad {
-    fn new(start_position: i32) -> Self {
+    fn new(start_position: [i32;2]) -> Self {
         Self {
             last_place: start_position,
+            keys : [[String::from(""),String::from(""),String::from("1"),String::from(""),String::from("")],
+                    [String::from(""),String::from("2"),String::from("3"),String::from("4"),String::from("")],
+                    [String::from("5"),String::from("6"),String::from("7"),String::from("8"),String::from("9")],
+                    [String::from(""),String::from("A"),String::from("B"),String::from("C"),String::from("")],
+                    [String::from(""),String::from(""),String::from("D"),String::from(""),String::from("")]]
         }
     }
     fn move_to(&mut self, instruction: Direction) {
         match instruction {
             Direction::Left => {
-                if (self.last_place % 3) - 1 != 0 {
-                    self.last_place -= 1
+                if self.last_place[1] != 0 && !self.keys.get(self.last_place[0] as usize).unwrap().get((self.last_place[1] - 1) as usize).unwrap().is_empty() {
+                    self.last_place[1] -= 1
                 }
             }
             Direction::Right => {
-                if (self.last_place % 3) + 1 != 1 {
-                    self.last_place += 1
+                if self.last_place[1] != 4 && !self.keys.get(self.last_place[0] as usize).unwrap().get((self.last_place[1] + 1) as usize).unwrap().is_empty() {
+                    self.last_place[1] += 1
                 }
             }
             Direction::Up => {
-                if (self.last_place - 3) > 0 {
-                    self.last_place -= 3
+                if self.last_place[0] != 0 && !self.keys.get((self.last_place[0] - 1) as usize).unwrap().get(self.last_place[1] as usize).unwrap().is_empty() {
+                    self.last_place[0] -= 1
                 }
             }
             Direction::Down => {
-                if (self.last_place + 3) < 10 {
-                    self.last_place += 3
+                if self.last_place[0] != 4 && !self.keys.get((self.last_place[0] + 1) as usize).unwrap().get(self.last_place[1] as usize).unwrap().is_empty() {
+                    self.last_place[0] += 1
                 }
             }
         }
@@ -59,11 +65,11 @@ impl KeyPad {
 }
 
 fn main() {
-    let mut key_pad = KeyPad::new(5);
+    let mut key_pad = KeyPad::new([2,0]);
     for lines in include_str!("input.txt").lines() {
         for direction in lines.chars() {
             key_pad.move_to(Direction::from_str(&direction.to_string()).unwrap())
         }
-        println!("digit: {}", key_pad.last_place)
+        println!("digit: {}", key_pad.keys.get(key_pad.last_place[0] as usize).unwrap().get(key_pad.last_place[1] as usize).unwrap())
     }
 }
