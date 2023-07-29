5f5a5a796f2e368a676d8ea5572cf9614bdb99c6
Toucan4Life <tdp94@hotmail.com>
2023-07-29 10:43:00 +0000

    Implement hashmap

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -1,69 +1,29 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 
-enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
-}
-
-impl From<char> for Direction {
-    fn from(s: char) -> Self {
-        match s {
-            'L' => Self::Left,
-            'R' => Self::Right,
-            'U' => Self::Up,
-            'D' => Self::Down,
-            _ => todo!(),
-        }
-    }
-}
-
-struct KeyPad<'a> {
-    keys: [[Option<&'a str>; 5]; 5],
-}
-
-impl KeyPad<'_> {
-    fn new(keys: [[Option<&str>; 5]; 5]) -> Self {
-        Self { keys }
-    }
-
-    fn move_to(&mut self, instruction: Direction, last_place: [i32; 2]) -> [i32; 2] {
-        match instruction {
-            Direction::Left  => if let Some(_) = self.try_moving([last_place[0], last_place[1] - 1]) { [last_place[0], last_place[1] - 1] } else { last_place },
-            Direction::Right => if let Some(_) = self.try_moving([last_place[0], last_place[1] + 1]) { [last_place[0], last_place[1] + 1] } else { last_place },
-            Direction::Up    => if let Some(_) = self.try_moving([last_place[0] - 1, last_place[1]]) { [last_place[0] - 1, last_place[1]] } else { last_place },
-            Direction::Down  => if let Some(_) = self.try_moving([last_place[0] + 1, last_place[1]]) { [last_place[0] + 1, last_place[1]] } else { last_place },
-        }
-    }
-
-    fn try_moving(&mut self, position: [i32; 2]) -> Option<&str> {
-        return *self.keys.get((position[0]) as usize)?.get((position[1]) as usize)?;
+fn apply_direction(instruction: char, current_position: [i32; 2]) -> [i32; 2] {
+    return match instruction {
+        'L' => [current_position[0], current_position[1] - 1],
+        'R' => [current_position[0], current_position[1] + 1],
+        'U' => [current_position[0] - 1, current_position[1]],
+        'D' => [current_position[0] + 1, current_position[1]],
+        char => panic!("incorrect input : {}", char)
     }
 }
 
 fn main() {
     let start_place = [2, 0];
-
-    let mut key_pad = KeyPad::new([
-        [None,  None,  Some("1"), None,  None],
-        [None,  Some("2"), Some("3"), Some("4"), None],
-        [Some("5"), Some("6"), Some("7"), Some("8"), Some("9")],
-        [None,  Some("A"),Some("B"),Some("C"), None],
-        [None,  None,  Some("D"), None,  None],
-    ]);
-
+    let key_pad = HashMap::from([([0,2], "1"), ([1,1], "2"), ([1,2], "3"), ([1,3], "4"), ([2,0], "5"), ([2,1], "6"), ([2,2], "7"), ([2,3], "8"), ([2,4], "9"), ([3,1], "A"), ([3,2], "B"), ([3,3], "C"), ([4,2], "D")]);
+    
     let code = include_str!("input.txt")
         .lines()
-        .map(|lines| {
-            let digit_position = lines.chars().fold(start_place, |acc, direction| {
-                key_pad.move_to(Direction::from(direction), acc)
+        .map(|lines| {            
+            let digit_position = lines.chars().fold(start_place, |current_position, direction| {
+                let new_position = apply_direction(direction,current_position);
+                if key_pad.contains_key(&new_position) { new_position } else { current_position }
             });
-            return if let Some(digit) = key_pad.try_moving(digit_position) {
-                digit
-            } else {
-                ""
-            };
+        
+            return key_pad.get(&digit_position).expect("We should always have this position");
         })
         .join("");
 
