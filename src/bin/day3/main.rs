d51f5ff4640fad1e812eb8c58dd7312c29161775
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2023-08-04 10:38:39 +0000

    day 3 part 2

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -1,17 +1,49 @@
 use itertools::Itertools;
 
 fn main() {
+    //part 1
     let code = include_str!("input.txt")
         .lines()
-        .filter(|line| {
-            let vec: Vec<i32> = [2, 7, 12]
+        .map(|line| {
+            [2, 7, 12]
                 .into_iter()
-                .map(|i| line[i..i + 3].trim_start().parse().unwrap())
+                .map(|i| line[i..i + 3].trim_start().parse::<i32>().unwrap())
                 .sorted()
-                .collect();
-            return vec[0] + vec[1] > vec[2];
+                .collect()
+        })
+        .filter(|row: &Vec<_>| row[0] + row[1] > row[2])
+        .count();
+
+    println!("{code}");
+
+    //part 2
+
+    let code = include_str!("input.txt")
+        .lines()
+        .map(|line| {
+            [2, 7, 12]
+                .into_iter()
+                .map(|i| line[i..i + 3].trim_start().parse::<i32>().unwrap())
+                .collect()
+        })
+        .tuples()
+        .flat_map(|(a, b, c)| transpose(vec![a, b, c]))
+        .filter(|row2| {
+            let mut row = row2.clone();
+            row.sort();
+            return row[0] + row[1] > row[2];
         })
         .count();
 
     println!("{code}");
+}
+
+fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>>
+where
+    T: Clone,
+{
+    assert!(!v.is_empty());
+    (0..v[0].len())
+        .map(|i| v.iter().map(|inner| inner[i].clone()).collect::<Vec<T>>())
+        .collect()
 }
