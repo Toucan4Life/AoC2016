57ec26b9424500c84671a091a0d0bd85bf000845
Toucan4Life <64911008+Toucan4Life@users.noreply.github.com>
2023-10-06 13:07:51 +0000

    day 5 part 2

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -1,6 +1,3 @@
-// use itertools::Itertools;
-// use std::str::FromStr;
-
 fn decrypt(input: String) -> String {
     let mut decrypted = "".to_string();
     for counter in 0.. {
@@ -17,12 +14,39 @@ fn decrypt(input: String) -> String {
     decrypted
 }
 
+fn decrypt2(input: String) -> String {
+    let mut decrypted: Vec<char> = "XXXXXXXX".chars().collect();
+    for counter in 0.. {
+        let hash = format!("{:x}", md5::compute(input.clone() + &counter.to_string()));
+        if hash.starts_with("00000") {
+            let pos = hash.chars().nth(5).unwrap();
+            if ['0', '1', '2', '3', '4', '5', '6', '7'].contains(&pos) {
+                let ch = hash.chars().nth(6).unwrap();
+                let posint = pos.to_digit(10).unwrap() as usize;
+                if decrypted[posint] == 'X' {
+                    decrypted[posint] = ch;
+                    println!("found char : {}", decrypted.iter().collect::<String>());
+                }
+            }
+        }
+        if !decrypted.contains(&'X') {
+            break;
+        }
+    }
+    decrypted.iter().collect()
+}
+
 fn main() {
-    let decrypted = decrypt(include_str!("input.txt").to_string());
+    let decrypted = decrypt2(include_str!("input.txt").to_string());
     println!("{decrypted}");
 }
 
 #[test]
 fn test_decrypt() {
     assert_eq!("18f47a30", decrypt("abc".to_string()));
+}
+
+#[test]
+fn test_decrypt2() {
+    assert_eq!("05ace8e3", decrypt2("abc".to_string()));
 }
